4cd6bfb5b16d1d0984d12324c5fdac6dda73a19b
Jahongir Ruzimboyev <webdevoloper97@gmail.com>
2025-06-26 05:43:27 +0500

    fix: enforce Python 3.11 for Render

--- middlewares/throttling.py
+++ middlewares/throttling.py
@@ -1,9 +1,11 @@
-import asyncio
-from aiogram import types, Dispatcher
+# middlewares/throttling.py
+from aiogram import types
 from aiogram.dispatcher import DEFAULT_RATE_LIMIT
 from aiogram.dispatcher.handler import CancelHandler, current_handler
 from aiogram.dispatcher.middlewares import BaseMiddleware
 from aiogram.utils.exceptions import Throttled
+
+from loader import dp as dispatcher
 
 class ThrottlingMiddleware(BaseMiddleware):
     def __init__(self, limit=DEFAULT_RATE_LIMIT, key_prefix='antiflood_'):
@@ -13,10 +15,9 @@ class ThrottlingMiddleware(BaseMiddleware):
 
     async def on_process_message(self, message: types.Message, data: dict):
         handler = current_handler.get()
-        dispatcher = Dispatcher.get_current()
         if handler:
-            limit = getattr(handler, 'throttling_rate_limit', self.rate_limit)
-            key = getattr(handler, 'throttling_key', f"{self.prefix}_{handler.__name__}")
+            limit = getattr(handler, "throttling_rate_limit", self.rate_limit)
+            key = getattr(handler, "throttling_key", f"{self.prefix}_{handler.__name__}")
         else:
             limit = self.rate_limit
             key = f"{self.prefix}_message"
