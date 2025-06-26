74d7cd8d51223b749ea1194d03a66da8f3c6ed02
Jahongir Ruzimboyev <webdevoloper97@gmail.com>
2025-06-26 05:46:46 +0500

    fix: enforce Python 3.11 for Render

--- middlewares/throttling.py
+++ middlewares/throttling.py
@@ -1,13 +1,17 @@
-# middlewares/throttling.py
-from aiogram import types
+import asyncio
+
+from aiogram import types, Dispatcher
 from aiogram.dispatcher import DEFAULT_RATE_LIMIT
 from aiogram.dispatcher.handler import CancelHandler, current_handler
 from aiogram.dispatcher.middlewares import BaseMiddleware
 from aiogram.utils.exceptions import Throttled
 
-from loader import dp as dispatcher
 
 class ThrottlingMiddleware(BaseMiddleware):
+    """
+    Simple middleware
+    """
+
     def __init__(self, limit=DEFAULT_RATE_LIMIT, key_prefix='antiflood_'):
         self.rate_limit = limit
         self.prefix = key_prefix
@@ -15,13 +19,13 @@ class ThrottlingMiddleware(BaseMiddleware):
 
     async def on_process_message(self, message: types.Message, data: dict):
         handler = current_handler.get()
+        dispatcher = Dispatcher.get_current()
         if handler:
             limit = getattr(handler, "throttling_rate_limit", self.rate_limit)
             key = getattr(handler, "throttling_key", f"{self.prefix}_{handler.__name__}")
         else:
             limit = self.rate_limit
             key = f"{self.prefix}_message"
-
         try:
             await dispatcher.throttle(key, rate=limit)
         except Throttled as t:
@@ -30,4 +34,4 @@ class ThrottlingMiddleware(BaseMiddleware):
 
     async def message_throttled(self, message: types.Message, throttled: Throttled):
         if throttled.exceeded_count <= 2:
-            await message.reply("⏱ Juda tez yozilyapti. Biroz kuting.")
+            await message.reply("Too many requests!")
